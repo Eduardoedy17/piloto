0a42933ca8ffc2637090382d757838a5395ee498
Eduardoedy17 <eduardoedy1717@gmail.com>
2025-11-28 20:57:51 -0300

    up_vercel

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Criação da view 'index'
+def index(request):
+    return HttpResponse("<h1>Olá, Mundo! Meu site Django está funcionando! 🚀</h1>")
