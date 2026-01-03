4e28b040774c4dbb1ddc95b60af341958d64ca25
Eduardoedy17 <eduardoedy1717@gmail.com>
2026-01-03 15:45:12 -0300

    .

--- pweb/settings.py
+++ pweb/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
 import home
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +76,7 @@ WSGI_APPLICATION = 'pweb.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgresql://neondb_owner:npg_2fmJpDOixQk1@ep-purple-grass-acmrwns1-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require')
 }
 
 
